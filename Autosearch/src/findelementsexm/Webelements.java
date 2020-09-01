5e1abe3ead6f91cad36f42b530fc323b9c588074
JaganM-QA <67819989+JaganM-QA@users.noreply.github.com>
2020-09-01 12:41:53 +0530

    Update Webelements.java

--- Autosearch/src/findelementsexm/Webelements.java
+++ Autosearch/src/findelementsexm/Webelements.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
+//Script created for automating Google Search and find respective results
 public class Webelements
 
 {
